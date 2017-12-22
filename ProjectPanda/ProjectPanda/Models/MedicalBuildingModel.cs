7f26ef0ada2ce8d7108258f44116b035ea6d1a86
Bonga <Bonga@DESKTOP-8R6GJO0>
2017-12-23 01:00:03 +0200

    new branch

---
+++ ProjectPanda/ProjectPanda/Models/MedicalBuildingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPanda.Models
+{
+    public class MedicalBuildingModel
+    {
+        public string HeadTitle { get; set; }
+        public string HeadLines { get; set; }
+        public string HeadLinesDesc { get; set; }
+        public string ProfileImage { get; set; }
+    }
+}
