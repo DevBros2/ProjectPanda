373c851c20cca3f133a02e8ae474e92e902c7290
Bonga <Bonga@DESKTOP-8R6GJO0>
2017-12-22 15:53:48 +0200

    model[DocAvaliable]

---
+++ ProjectPanda/ProjectPanda/Models/DocAvaliable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ProjectPanda.Models
+{
+    public class DocAvaliable : INotifyPropertyChanged
+    {
+       
+        private string Name;
+        private string Varsity;
+
+        public string name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string varsity
+        {
+            get { return varsity; }
+            set
+            {
+                Varsity = value;
+                OnPropertyChanged("Varsity");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+
+        
+        
+    }
+}
