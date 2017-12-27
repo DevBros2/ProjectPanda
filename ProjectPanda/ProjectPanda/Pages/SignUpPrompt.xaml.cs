67a06d18230c7af1b2da4f9f2e75bcf4ca923e44
Bonga <Bonga@DESKTOP-8R6GJO0>
2017-12-27 09:42:30 +0200

    SAVE

--- ProjectPanda/ProjectPanda/Pages/SignUpPrompt.xaml.cs
+++ ProjectPanda/ProjectPanda/Pages/SignUpPrompt.xaml.cs
@@ -16,5 +16,29 @@ namespace ProjectPanda.Pages
 		{
 			InitializeComponent ();
 		}
-	}
+        private void SignUpGoogle(object sender, EventArgs e)
+        {
+            //The following code makes instasiates the class
+           // Authentication authentication = new Authentication();
+            //authentication.GoogeleAuthFunction();
+        }
+
+        private void signUpFacebook(object sender, EventArgs e)
+        {
+            //Authentication authentication = new Authentication();
+            //authentication.FacebookAuthFunction();
+        }
+
+        private void signUpOutlook(object sender, EventArgs e)
+        {
+            //Authentication authentication = new Authentication();
+            //authentication.MicrosoftAuthFunction();
+        }
+
+        private async void backbutton(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new Login());
+        }
+
+    }
 }
