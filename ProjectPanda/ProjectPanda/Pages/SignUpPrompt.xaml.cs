8033249ad8a1ab8479be75311d74f14bfe5d9cb8
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2017-12-27 11:35:00 +0200

    Revert here if we have anymore errors going forward

--- ProjectPanda/ProjectPanda/Pages/SignUpPrompt.xaml.cs
+++ ProjectPanda/ProjectPanda/Pages/SignUpPrompt.xaml.cs
@@ -16,26 +16,26 @@ namespace ProjectPanda.Pages
 		{
 			InitializeComponent ();
 		}
-        private void SignUpGoogle(object sender, EventArgs e)
+        private void GoogleSignIn(object sender, EventArgs e)
         {
             //The following code makes instasiates the class
            // Authentication authentication = new Authentication();
             //authentication.GoogeleAuthFunction();
         }
 
-        private void signUpFacebook(object sender, EventArgs e)
+        private void FacebookSignIn(object sender, EventArgs e)
         {
             //Authentication authentication = new Authentication();
             //authentication.FacebookAuthFunction();
         }
 
-        private void signUpOutlook(object sender, EventArgs e)
+        private void OutlookSignIn(object sender, EventArgs e)
         {
             //Authentication authentication = new Authentication();
             //authentication.MicrosoftAuthFunction();
         }
 
-        private async void backbutton(object sender, EventArgs e)
+        private async void OurSignIn(object sender, EventArgs e)
         {
             //await Navigation.PushAsync(new Login());
         }
