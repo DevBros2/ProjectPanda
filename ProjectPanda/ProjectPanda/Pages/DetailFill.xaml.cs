4a16ab6fc2b0888266ce75f64700e3b5e8f09483
Bonga <bonga.developer@outlook.com>
2018-01-05 13:04:24 +0200

    macbook

---
+++ ProjectPanda/ProjectPanda/Pages/DetailFill.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ProjectPanda.Pages
+{
+    public partial class DetailFill : ContentPage
+    {
+        public DetailFill()
+        {
+            InitializeComponent();
+        }
+    }
+}
