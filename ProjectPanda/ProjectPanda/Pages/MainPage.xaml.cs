9896b794bb0c44a92d375c6062078c0ca2a01713
Bonga <Bonga@DESKTOP-8R6GJO0>
2018-01-15 11:22:21 +0200

    doctorlistview

--- ProjectPanda/ProjectPanda/Pages/MainPage.xaml.cs
+++ ProjectPanda/ProjectPanda/Pages/MainPage.xaml.cs
@@ -1,11 +1,11 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ProjectPanda.Views;
+
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +15,7 @@ namespace ProjectPanda.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : MasterDetailPage
     {
+        
         public MainPage()
         {
             InitializeComponent();
