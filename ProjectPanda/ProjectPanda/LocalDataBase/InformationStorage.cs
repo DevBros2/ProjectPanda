61697558a4d93d3d84e72b133358355c724cd21d
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2018-01-19 23:44:28 +0200

    still doing the local db

---
+++ ProjectPanda/ProjectPanda/LocalDataBase/InformationStorage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using System.Threading.Tasks;
+
+namespace ProjectPanda.LocalDataBase
+{
+     public  class InformationStorage
+    {
+        public async Task SaveUserInformation()
+        {
+            List<User> listedInformation;
+           // listedInformation=await dbConn
+        }
+
+    }
+}
