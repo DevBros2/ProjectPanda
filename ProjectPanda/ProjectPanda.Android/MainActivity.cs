6e6c98c0b80a6d1bea9a87720f95f100674ab1f9
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2018-01-25 16:48:51 +0200

    Trying to get the geolocation working

--- ProjectPanda/ProjectPanda.Android/MainActivity.cs
+++ ProjectPanda/ProjectPanda.Android/MainActivity.cs
@@ -11,19 +11,21 @@ using System.Threading.Tasks;
 using Microsoft.WindowsAzure.MobileServices.Sync;
 using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
 using SQLitePCL.Extensions;
-
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using Xamarin.Forms.Platform.Android;
 
 namespace ProjectPanda.Droid
 {
-	[Activity (Label = "ProjectPanda", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-	{
-		protected override void OnCreate (Bundle bundle)
-		{
-			TabLayoutResource = Resource.Layout.Tabbar;
-			ToolbarResource = Resource.Layout.Toolbar; 
+    [Activity(Label = "ProjectPanda", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            FormsAppCompatActivity.TabLayoutResource = Resource.Layout.Tabbar;
+            FormsAppCompatActivity.ToolbarResource = Resource.Layout.Toolbar;
 
-			base.OnCreate (bundle);
+            base.OnCreate(bundle);
 
             // This MobileServiceClient has been configured to communicate with the Azure Mobile App and
             // Azure Gateway using the application url. You're all set to start working with your Mobile App!
@@ -34,9 +36,15 @@ namespace ProjectPanda.Droid
             global::Xamarin.Auth.Presenters.XamarinAndroid.AuthenticationConfiguration.Init(this, bundle);
 
 
-            global::Xamarin.Forms.Forms.Init (this, bundle);
-			LoadApplication (new ProjectPanda.App ());
-		}
-	}
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new ProjectPanda.App());
+
+
+        }
+
+
+    
+    }
 }
 
---
+++ ProjectPanda/ProjectPanda/Models/Locator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPanda.Models
+{
+    public class Locator
+    {
+        public void GetLocation()
+        {
+
+        }
+    }
+}
