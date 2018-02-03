cc52a0ed98d5f2817099ff7f0b733bbf0cf5ce8c
Bonga <bonga.developer@outlook.com>
2018-02-03 16:40:04 +0200

    Revert "Real prototype"

--- ProjectPanda/ProjectPanda/ViewModels/MedicalBuildingViewModel.cs
+++ ProjectPanda/ProjectPanda/ViewModels/MedicalBuildingViewModel.cs
@@ -22,12 +22,14 @@ namespace ProjectPanda.ViewModels
             Practices = new ObservableCollection<MedicalBuildingModel>
             {
                 new MedicalBuildingModel{ ProfileImage ="GreysHospital.jpg",PracticeName = "Grey's Hospital",Distance = 6.2,},
-                new MedicalBuildingModel{ ProfileImage ="CromptonHospital.jpg",PracticeName = "CromptomHospital",Distance = 8,},
-                new MedicalBuildingModel{ ProfileImage ="GrooteSchuurHospital.jpg",PracticeName = "GrooteSchuurHospital",Distance = 9.7,},
-                new MedicalBuildingModel{ ProfileImage ="PinetownClinic.jpg",PracticeName = "PinetownClinic",Distance = 13.3,},
-                new MedicalBuildingModel{ ProfileImage ="WestvilleHospital.jpg",PracticeName = "WestvilleHospital",Distance = 15.0,},
-                new MedicalBuildingModel{ ProfileImage ="Medicross_Pinetown.jpg",PracticeName = "Medicross:Pinetown",Distance = 18.5,},
-                new MedicalBuildingModel{ ProfileImage ="GreysHospital.jpg", PracticeName = "Grey's Hospital", Distance = 20.2,},
+                new MedicalBuildingModel{ ProfileImage ="GreysHospital.jpg",PracticeName = "CromptomHospital",Distance = 8,},
+                new MedicalBuildingModel{ ProfileImage ="GreysHospital.jpg",PracticeName = "GrooteSchuurHospital",Distance = 9.7,},
+                new MedicalBuildingModel{ ProfileImage ="GreysHospital.jpg",PracticeName = "PinetownClinic",Distance = 13.3,},
+                new MedicalBuildingModel{ ProfileImage ="GreysHospital.jpg",PracticeName = "WestvilleHospital",Distance = 15.0,},
+                new MedicalBuildingModel{ ProfileImage ="GreysHospital.jpg",PracticeName = "Medicross:Pinetown",Distance = 18.5,},
+                new MedicalBuildingModel{ ProfileImage="GreysHospital.jpg", PracticeName = "Grey's Hospital", Distance = 20.2,},
+
+
             };
 
         }
