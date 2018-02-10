4c88b28e8a1ac6aa1028f1e104268d7de7972b09
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2018-02-11 01:30:43 +0200

    still adding more code for the history

---
+++ ProjectPanda/ProjectPanda/Models/DeliveryModels/PrecriptionHistory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProjectPanda.Models;
+
+namespace ProjectPanda.Models.DeliveryModels
+{
+    public class PrecriptionHistory: PrescribedMedication
+    {
+
+        public string _medicationHistory;
+        public string _dosageHistory;
+        public string _descriptionHistory;
+
+        //property to  get and set to the histroy listview 
+        public string MedicationNameHistory
+        {
+            get { return _medicationHistory; }
+
+            set { _medicationHistory = Medication; }
+            
+        }
+            
+
+        //property to get and set to the history listview
+       public string MedicationDosageHistory
+        {
+            get { return _dosageHistory; }
+
+            set { _dosageHistory = DosageAmount; }
+        }
+
+
+        public string DescriptionHistory
+        {
+            get { return _descriptionHistory; }
+
+            set { _descriptionHistory = DescriptionHistory; }
+        }
+
+
+    }
+}
---
+++ ProjectPanda/ProjectPanda/ViewModels/DeliveryViewModels/PrescriptionHistroyViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using ProjectPanda.Models.DeliveryModels;
+using ProjectPanda.Views;
+
+namespace ProjectPanda.ViewModels.DeliveryViewModels
+{
+
+    
+
+    public class PrescriptionHistroyViewModel
+    {
+
+        public ObservableCollection<PrecriptionHistory> _historyPrescriptionList { get; set; }
+        public object SelectedItem { get; set; }
+
+        public PrescriptionHistroyViewModel()
+        {
+            GenerateHistoryList();
+        }
+
+        private void GenerateHistoryList()
+        {
+            _historyPrescriptionList = new ObservableCollection<PrecriptionHistory>
+            {
+
+                //get the values from the cloud 
+
+            };
+        }
+    }
+}
