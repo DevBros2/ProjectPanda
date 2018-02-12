f03313a9e1935df2bb7a1a7ebb59938ff0c41962
Bonga <Bonga@DESKTOP-8R6GJO0>
2018-02-13 01:28:28 +0200

    null changes

--- ProjectPanda/ProjectPanda/ViewModels/AppointmentViewModels/MyAppointmentsViewModel.cs
+++ ProjectPanda/ProjectPanda/ViewModels/AppointmentViewModels/MyAppointmentsViewModel.cs
@@ -10,6 +10,8 @@ namespace ProjectPanda.ViewModels
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        //please implement it here
+
         private void OnPropertyChanged(string propertyName)
         {
             if (PropertyChanged != null)
--- ProjectPanda/ProjectPanda/Views/AppoinmentViews/MyAppointments.xaml.cs
+++ ProjectPanda/ProjectPanda/Views/AppoinmentViews/MyAppointments.xaml.cs
@@ -18,6 +18,8 @@ namespace ProjectPanda.Views
 		public MyAppointments ()
 		{
 			InitializeComponent ();
+
+            BindingContext = _viewmodel = new MyAppointmentsViewModel();
 		}
 
 
