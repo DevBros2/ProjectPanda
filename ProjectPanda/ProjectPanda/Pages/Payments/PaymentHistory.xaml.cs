acc55c12f5d9f145aeea1282a7cf597b6eb41752
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2018-02-28 21:28:48 +0200

    .

--- ProjectPanda/ProjectPanda/Pages/Payments/PaymentHistory.xaml.cs
+++ ProjectPanda/ProjectPanda/Pages/Payments/PaymentHistory.xaml.cs
@@ -18,6 +18,28 @@ namespace ProjectPanda.Pages
             //removes the second Navbar 
             NavigationPage.SetHasNavigationBar(this, false);
         }
+
+        #region Pick a payment option
+        private void PickPaymentOption(object sender, EventArgs e)
+        {
+            //the code to go to the paymant option selection and updating credt/debit information
+
+        }
+        #endregion
+
+        #region View the payment history
+        private void ViewPaymentHistory(object sender, EventArgs e)
+        {
+            //The code for the vieng for the payment history
+        }
+        #endregion
+
+        #region Manage the subscriptions
+        private void ManageSubscription(object sender, EventArgs e)
+        {
+            //The code for managing the subscriptions for chronic diseases 
+        }
+        #endregion
     }
-	
+
 }
---
+++ ProjectPanda/ProjectPanda/Pages/Payments/PaymentOptions.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectPanda.Pages.Payments
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PaymentOptions : ContentPage
+	{
+		public PaymentOptions ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
