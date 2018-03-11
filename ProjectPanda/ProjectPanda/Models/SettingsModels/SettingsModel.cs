b80dd8583816ea47d78cf1b3f347c5d24a74cad2
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2018-03-12 01:45:02 +0200

    adde more work properties

--- ProjectPanda/ProjectPanda/Models/SettingsModels/SettingsModel.cs
+++ ProjectPanda/ProjectPanda/Models/SettingsModels/SettingsModel.cs
@@ -21,5 +21,7 @@ namespace ProjectPanda.Models
         public String WorkAddressLine2 { get; set; }
         public String WorkCityOrTown { get; set; }
         public String WorkPostal_Code { get; set; }
+        public Boolean isWorkDeliveryEnabled { get; set; }
+
     }
 }
---
+++ ProjectPanda/ProjectPanda/Views/CustomViews/CustomBoolEntryCell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectPanda.Views.CustomViews
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CustomBoolEntryCell : ViewCell
+	{
+		public CustomBoolEntryCell ()
+		{
+			InitializeComponent ();
+		}
+
+        CustomBoolEntryCell switcher = new CustomBoolEntryCell();
+        
+    }
+}
