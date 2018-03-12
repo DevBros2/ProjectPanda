93fa1a2767267de5ee2bf4321b6e546c1ea8089b
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2018-03-12 19:45:02 +0200

    sdf

---
+++ ProjectPanda/ProjectPanda/Models/AppointmentModels/ProblemEntryModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProjectPanda.ViewModels.AppointmentViewModels;
+
+namespace ProjectPanda.Models.AppointmentModels
+{
+     public class ProblemEntryModel
+    {
+
+    }
+}
---
+++ ProjectPanda/ProjectPanda/ViewModels/AppointmentViewModels/ProblemEntryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProjectPanda.Models.AppointmentModels;
+using ProjectPanda.Views.AppoinmentViews;
+using Xamarin.Forms;
+
+namespace ProjectPanda.ViewModels.AppointmentViewModels
+{
+    public class ProblemEntryViewModel:Editor
+    {
+       
+    }
+}
