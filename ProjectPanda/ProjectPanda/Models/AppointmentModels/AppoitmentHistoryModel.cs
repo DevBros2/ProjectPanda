ef5416b857390a237b8e42ad319bfe768402ff61
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2018-03-12 15:58:51 +0200

    added the scroll view for the problem entry text feild for the user to enter their information

--- ProjectPanda/ProjectPanda/Models/AppointmentModels/AppoitmentHistoryModel.cs
+++ ProjectPanda/ProjectPanda/Models/AppointmentModels/AppoitmentHistoryModel.cs
@@ -6,8 +6,47 @@ namespace ProjectPanda.Models
 {
       public class AppoitmentHistoryModel
     {
-        public string date { get; set; }
-        public string time { get; set; }
-        public string doctorSeen { get; set; }
+        private string dateseen;
+        private string time;
+        private string doctorSeen;
+
+        public string DateSeen
+        {
+            get
+            {
+                return dateseen;
+            }
+
+            set
+            {
+                dateseen = value;
+            }
+        }
+
+
+        public string Time
+        {
+            get
+            {
+                return time;
+            }
+            set
+            {
+                time = value;
+            }
+        }
+
+
+        public string DoctorSeen
+        {
+            get
+            {
+                return doctorSeen;
+            }
+            set
+            {
+                doctorSeen = value;
+            }
+        }
     }
 }
--- ProjectPanda/ProjectPanda/ViewModels/AppointmentViewModels/AppoitmentHistoryViewModel.cs
+++ ProjectPanda/ProjectPanda/ViewModels/AppointmentViewModels/AppoitmentHistoryViewModel.cs
@@ -23,9 +23,9 @@ namespace ProjectPanda.ViewModels
         {
             _History = new ObservableCollection<AppoitmentHistoryModel>
             {
-                new AppoitmentHistoryModel{date="21/01/2018", doctorSeen="Dr K.L Buthelezi", time="09h23m"},
-                new AppoitmentHistoryModel{date="03/09/2017", doctorSeen="Dr R Rox", time="14h00m" },
-                new AppoitmentHistoryModel{date="21/01/2018", doctorSeen="Dr A Xulu", time="09h23m"}
+                new AppoitmentHistoryModel{DateSeen="21/01/2018", DoctorSeen="Dr K.L Buthelezi", Time="09h23m"},
+                new AppoitmentHistoryModel{DateSeen="03/09/2017", DoctorSeen="Dr R Rox", Time="14h00m" },
+                new AppoitmentHistoryModel{DateSeen="21/01/2018", DoctorSeen="Dr A Xulu", Time="09h23m"}
             };
         }
 
