c61b00003dfc509c450ccbe690f47dc9b4b1cf50
Bonga <bonga.developer@outlook.com>
2018-06-19 19:14:44 +0200

    mongo services was added

---
+++ ProjectPanda/ProjectPanda/Services/MongoDBResp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectPanda.Models;
+using MongoDB.Driver;
+
+namespace ProjectPanda.Services
+{
+    public class MongoDBResp
+    {
+        readonly static string connectionString = "mongodb://localhost:27017";//for local development
+        readonly static string DatabaseCall = "database"; //making it readonly so cant be attacked easily
+        readonly static string CollectionCall = "Collection";
+
+        private IMongoClient _client;
+        private IMongoDatabase _database;
+        IMongoCollection<DocAvaliable> _doctorCollection;
+
+        /// <summary>
+
+        /// Checking is connection to the database established.
+
+        /// </summary>
+        /// 
+        public bool CheckConnection()//method for checking connectivity before attempt to CRUD data.
+        {
+            try
+            {
+                _database.ListCollections();
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public MongoDBResp(string connectionString)
+        {
+
+            _client = new MongoClient(connectionString);
+
+            _database = _client
+                .GetDatabase(DatabaseCall);
+
+            _doctorCollection = _database
+                .GetCollection<DocAvaliable>(CollectionCall); 
+            
+        }
+        public async Task<List<DocAvaliable>> GetUsersBySpecialization(string medicalbuilding, string docSpecialization )
+        {
+            //filter function, this will filter by Medical building and specialization
+            var filter = Builders<DocAvaliable>
+                .Filter
+                .Eq(medicalbuilding, docSpecialization);
+            //adds filter to the query
+            var result = await _doctorCollection
+                .Find(filter)
+                .ToListAsync();//retiurns it as a list
+            return result;
+        }
+
+
+    }
+}
--- ProjectPanda/ProjectPanda/ViewModels/MedicalBuildingViewModel.cs
+++ ProjectPanda/ProjectPanda/ViewModels/MedicalBuildingViewModel.cs
@@ -32,20 +32,14 @@ namespace ProjectPanda.ViewModels
                 new MedicalBuildingModel{Distance=7.8, ProfileImage ="GreysHospital.jpg",PracticeName = "Grey's Hospital",ChartData = Chart1, PatientsCurrentlyAtPractice=5},
                 new MedicalBuildingModel{Distance=7.0, ProfileImage ="GreysHospital.jpg",PracticeName = "CromptomHospital",ChartData = Chart2, PatientsCurrentlyAtPractice=3},
                 new MedicalBuildingModel{Distance=6.0, ProfileImage ="GreysHospital.jpg",PracticeName = "GrooteSchuurHospital",ChartData = Chart3, PatientsCurrentlyAtPractice=12},
-                new MedicalBuildingModel{Distance=12.5,  ProfileImage ="GreysHospital.jpg",PracticeName = "PinetownClinic",ChartData = Chart4, PatientsCurrentlyAtPractice=20},
+                new MedicalBuildingModel{Distance=12.5,ProfileImage ="GreysHospital.jpg",PracticeName = "PinetownClinic",ChartData = Chart4, PatientsCurrentlyAtPractice=20},
                 new MedicalBuildingModel{Distance=8.0, ProfileImage ="GreysHospital.jpg",PracticeName = "WestvilleHospital",ChartData = Chart5, PatientsCurrentlyAtPractice=8},
                 new MedicalBuildingModel{Distance=5.5, ProfileImage ="GreysHospital.jpg",PracticeName = "Medicross:Pinetown",ChartData = Chart6, PatientsCurrentlyAtPractice=11},
                 new MedicalBuildingModel{Distance=2.5, ProfileImage ="GreysHospital.jpg",PracticeName = "Grey's Hospital",ChartData = Chart7, PatientsCurrentlyAtPractice=15},
-
-       
-
             };
 
             //ChartValue = this.ChartValue;
-
             /*
-                             
-         
              */
         }
 
